902d0230b5e41b0e5e83b8581c755a884291984d
Christopher Martin <christopher@orudo.cc>
2021-02-17 13:07:29 +0900

    discounting almost finished

--- reedbush-scripts/run-on-ist.sh
+++ reedbush-scripts/run-on-ist.sh
@@ -5,8 +5,9 @@ while [ "$(squeue -u u00222| wc -l)" -gt 10 ]
 do
   sleep 1
 done
-cat ./$1 | grep -o "singularity exec .*" | sed 's/\/lustre\/gk77\/k77015\/.Singularity\/imgs\/LISA.simg/\/home\/u00222\/singularity\/images\/LISA.simg/' | sed 's/&>/>/' | while read line; do echo "-p p --gres=gpu:1 --mem=24GB" $line "2>&1 &"; done
-cat ./$1 | grep -o "singularity exec .*" | sed 's/\/lustre\/gk77\/k77015\/.Singularity\/imgs\/LISA.simg/\/home\/u00222\/singularity\/images\/LISA.simg/' | sed 's/&>/>/' | while read line; do echo "-p p --gres=gpu:1 --mem=24GB" $line "2>&1 &"; done | xargs -n 18 -P 4 -L 1 srun
+#cat ./$1 | grep -o "singularity exec .*" | sed 's/\/lustre\/gk77\/k77015\/.Singularity\/imgs\/LISA.simg/\/home\/u00222\/singularity\/images\/LISA.simg/' | while read line; do echo "-p p --gres=gpu:1 --mem=24GB" $line ; done | xargs -n 16 -I '{}' echo "srun {} guard"
+cat ./$1 | grep -o "singularity exec .*" | sed 's/\/lustre\/gk77\/k77015\/.Singularity\/imgs\/LISA.simg/\/home\/u00222\/singularity\/images\/LISA.simg/' | while read line; do echo "-p p --gres=gpu:1 --mem=24GB" $line ; done | xargs -n 16 -P 4 -L 1 -I '{}' bash -c "srun {}"
+#cat ./$1 | grep -o "singularity exec .*" | sed 's/\/lustre\/gk77\/k77015\/.Singularity\/imgs\/LISA.simg/\/home\/u00222\/singularity\/images\/LISA.simg/' | while read line; do echo "-p p --gres=gpu:1 --mem=24GB" $line ; done | xargs -n 16 -P 4 -L 1 srun
 
 #cat ./$1 | grep -o "singularity exec (.)*"
 
