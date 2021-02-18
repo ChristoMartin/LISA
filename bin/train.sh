fd06f7e7218fdc0251e91ae3c457acf1c20119f6
christopher <christopher@orudo.cc>
2021-02-18 16:22:57 +0900

    bug fix for generating stripped matrix

--- bin/train.sh
+++ bin/train.sh
@@ -23,8 +23,8 @@ then
   echo $additional_params
 fi
 echo "python3 src/train.py --train_files $train_files   --dev_files $dev_files   --transition_stats $transition_stats   --data_config $data_config --attention_configs $attention_configs  --model_configs $model_configs   --task_configs $task_configs   --layer_configs $layer_configs   --best_eval_key $best_eval_key   $params $additional_params "
-save_dir=$(grep -e "save_dir [^\ ]" $params)
-echo $save_dir
+#save_dir=$(grep -e "save_dir [^\ ]" $params)
+#echo $save_dir
 
 
 python3 src/train.py \
@@ -40,7 +40,7 @@ $params \
 $additional_params
 
 
-echo "find  -regex '.*best_exporter/[0-9]+' | xargs -n 1 -I {} singularity exec --nv /lustre/gk77/k77015/.Singularity/imgs/LISA.simg bin/evaluate-exported.sh config/lisa-discounting/conll05-lisa.conf --save_dir {} 2> mypipe | grep srl_f1 mypipe"
+#echo "find  -regex '.*best_exporter/[0-9]+' | xargs -n 1 -I {} singularity exec --nv /lustre/gk77/k77015/.Singularity/imgs/LISA.simg bin/evaluate-exported.sh config/lisa-discounting/conll05-lisa.conf --save_dir {} 2> mypipe | grep srl_f1 mypipe"
 
 #if [ -z "$attention_configs" ]
 #then
--- reedbush-scripts/run-lisa-tbatching-injection-linear-aggregation-dep&local-singlehead.sh
+++ reedbush-scripts/run-lisa-tbatching-injection-linear-aggregation-dep&local-singlehead.sh
@@ -14,11 +14,11 @@ cd $PBS_O_WORKDIR
 
 module add cuda9/9.0.176-cuDNN7.1.4 singularity/2.5.1 
 {
-CUDA_VISIBLE_DEVICES=0 singularity exec --nv /lustre/gk77/k77015/.Singularity/imgs/LISA.simg bin/train.sh config/lisa-aggregations-injection/conll05-linear-aggregation-dep\&local-singlehead-10layers-base.conf --save_dir .model/.model-lisa-injection-linear-aggregation/run-singlehead-dep\&local-base/run-1 --num_gpus 1 &> .log/conll05-lisa-10layers-base-tbatching-injection-linear-aggregation-dep\&local-singlehead-run-1.log
+CUDA_VISIBLE_DEVICES=0 singularity exec --nv /lustre/gk77/k77015/.Singularity/imgs/LISA.simg bin/train.sh config/lisa-aggregations-injection/conll05-linear-aggregation-depnlocal-singlehead-10layers-base.conf --save_dir .model/.model-lisa-injection-linear-aggregation/run-singlehead-depnlocal-base/run-1 --num_gpus 1 &> .log/conll05-lisa-10layers-base-tbatching-injection-linear-aggregation-depnlocal-singlehead-run-1.log
 } & 
 
 {
 sleep 160
-CUDA_VISIBLE_DEVICES=1 singularity exec --nv /lustre/gk77/k77015/.Singularity/imgs/LISA.simg bin/train.sh config/lisa-aggregations-injection/conll05-linear-aggregation-dep\&local-singlehead-10layers-base.conf --save_dir .model/.model-lisa-injection-linear-aggregation/run-singlehead-dep\&local-base/run-2 --num_gpus 1 &> .log/conll05-lisa-10layers-base-tbatching-injection-linear-aggregation-dep\&local-singlehead-run-2.log
+CUDA_VISIBLE_DEVICES=1 singularity exec --nv /lustre/gk77/k77015/.Singularity/imgs/LISA.simg bin/train.sh config/lisa-aggregations-injection/conll05-linear-aggregation-depnlocal-singlehead-10layers-base.conf --save_dir .model/.model-lisa-injection-linear-aggregation/run-singlehead-depnlocal-base/run-2 --num_gpus 1 &> .log/conll05-lisa-10layers-base-tbatching-injection-linear-aggregation-depnlocal-singlehead-run-2.log
 } &
 wait
